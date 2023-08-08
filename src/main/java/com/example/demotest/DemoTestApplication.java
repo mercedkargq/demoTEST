ca01d06d8dc82a5329c4f95327ea78aaacd9943c
hualing.xue <97194915@qq.com>
2023-08-08 14:23:25 +0800

    我的测试

---
+++ src/main/java/com/example/demotest/DemoTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestApplication {
+
+    public static void main(String[] args) {
+        System.out.println("2222");
+        SpringApplication.run(DemoTestApplication.class, args);
+    }
+
+}
